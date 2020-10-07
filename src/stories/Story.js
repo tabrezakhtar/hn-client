b387eb2d0276cd6ae9b80c1809f5fde2e403ba8e
tabrezakhtar <tabrez.akhtar1@gmail.com>
2020-10-07 15:02:49 +0100

    add moment

--- src/stories/Story.js
+++ src/stories/Story.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
 function Story({story, index}) {
+  const formattedDate = moment(story.created_at).fromNow();
   return <li className="collection-item">
     <span className="story__number">{index+1}</span>
     <a className="story__link" href={'/comments/' + story.objectID}>{story.title}</a>
-    <span className="comments__number">15 comments</span>
-    <span className="story__time">10 minutes ago</span>
+    <span className="comments__number">{story.num_comments} comments</span>
+    <span className="story__time">{formattedDate}</span>
   </li>;
 }
 
